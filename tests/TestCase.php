5711c49fa76f87b293a8dc695cf022918bdc95cc
erikn69 <erikn_69@hotmail.com>
2022-11-14 09:36:28 -0500

    Fix testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,6 +5,7 @@
 	namespace Fawno\FPDF\Tests;
 
 	use ddn\sapp\PDFDoc;
+	use ddn\sapp\pdfvalue\PDFValue;
 	use FPDF;
 	use PHPUnit\Framework\TestCase as PHPUnitTestCase;
 	use ReflectionClass;
@@ -45,16 +46,19 @@
 
 		public function assertPdfAreEquals (string $expected, string $actual) : void {
 			$doc_expected = PDFDoc::from_string($expected);
-			$this->assertIsObject($doc_expected);
+			$this->assertIsObject($doc_expected, 'The expected file can\'t be parsed as PDF.');
 
 			$doc_actual = PDFDoc::from_string($actual);
-			$this->assertIsObject($doc_actual);
+			$this->assertIsObject($doc_actual, 'The actual file can\'t be parsed as PDF.');
 
 			$differences = $doc_expected->compare($doc_actual);
+
+			$diff = [];
 			foreach ($differences as $oid => $obj) {
-				foreach ($obj->get_keys() as $key) {
-					$this->assertTrue(in_array($key, ['Producer', 'CreationDate']));
-				}
+				$keys = (is_a($obj->get_value(), PDFValue::class) ? $obj->get_keys() : false) ?: ['OID_' . $obj->get_oid()];
+				$diff = array_merge($diff, array_diff($keys, ['Producer', 'CreationDate']));
 			}
+
+			$this->assertEquals([], $diff, 'The PDFs contents have differences.');
 		}
 	}
